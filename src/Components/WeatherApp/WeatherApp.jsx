e5c53cc6a11ae1d378f4e92b2c32f499cf222741
mumble <andreapauline.deguzman@gmail.com>
2023-11-06 10:27:53 +0800

    initial commit for weather-app

---
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './WeatherApp.css';
+
+import search_icon from '../Assets/search.png';
+import clear_icon from '../Assets/clear.png';
+import cloudy_icon from '../Assets/cloud.png';
+import drizzle_icon from '../Assets/drizzle.png';
+import humid_icon from '../Assets/humidity.png';
+import rain_icon from '../Assets/rain.png';
+import snow_icon from '../Assets/snow.png';
+import wind_icon from '../Assets/wind.png';
+
+export const WeatherApp = () => {
+  return (
+    <div>WeatherApp</div>
+  )
+}
